f1cf7387f69bde9dd7f717ae9f172f42d9d4b3d4
Blehjo <blehjooo@gmail.com>
2021-10-30 21:40:00 -0500

    Javascript Update

--- assets/script.js
+++ assets/script.js
@@ -13,42 +13,6 @@
 // THEN the saved events persist
 
 //Let's make a timeblock
-// var col9El = document.getElementById('time9');
-// var col10El = document.getElementById('time10');
-// var col11El = document.getElementById('col11');
-// var col12El = document.getElementById('col12');
-// var col1El = document.getElementById('col1');
-// var col2El = document.getElementById('col2');
-// var col3El = document.getElementById('col3');
-// var col4El = document.getElementById('col4');
-// var col5El = document.getElementById('time5');
-
-var hourColumnEl = document.querySelectorAll('.col-3.hour')
-
-var currentDayEl = document.getElementById('currentDay');
-
-function getCurrentDate() {
-    var date = moment().format('MMMM Do YYYY');
-    console.log(date);
-    currentDayEl.textContent = date;
-} 
-
-function changeTense () {
-    var date = moment().format('LT');
-    for (i = 0; i < hourColumnEl.length; i++) {
-        hourColumnEl[i].textContent = date;
-        if (hourColumnEl[i] === date) {
-            allTimeBlocks[i].textContent('future');
-            // allTimeBlocks[i].classList.add('present');
-        }
-    };
-}
-
-$('document').ready(getCurrentDate);
-$('document').ready(changeTense);
-
-//We want to call the time column and be able to add a class to it, so that we can apply css to it.
-
 var time9El = document.getElementById('time9');
 var time10El = document.getElementById('time10');
 var time11El = document.getElementById('time11');
@@ -65,9 +29,46 @@ for (i = 0; i < allTimeBlocks.length; i++) {
     allTimeBlocks[i].classList.add('future')
 }
 
+var hourColumn = document.querySelectorAll('.hour')
+var hourColumnEl = Array.from(hourColumn);
+var currentDayEl = document.getElementById('currentDay');
+
+function getCurrentDate() {
+    var date = moment().format('MMMM Do YYYY');
+    console.log(date);
+    currentDayEl.textContent = date;
+} 
+
+$('document').ready(getCurrentDate);
+
+//We want to call the time column and be able to add a class to it, so that we can apply css to it.
+
+function changeTense () {
+    var date = new Date().getHours();
+    console.log(date);
+    for (i = 0; i < hourColumnEl.length; i++) {
+        console.log(hourColumn[i].value);
+        // hourColumnEl[i].textContent = date;
+        if (hourColumnEl[i].value == date) {
+            allTimeBlocks[i].classList.remove('future');
+            allTimeBlocks[i].classList.add('present');
+            console.log(hourColumn[i].textContent);
+        }
+    };
+}
+
+$('document').ready(changeTense);
+
 // var time = moment.locale(); //en
 var trees = moment().format('LT');   // 5:26 PM
 console.log(trees);
 var today = new Date();
 var date = moment().format('LT');
 
+
+var now= new Date().getHours();
+console.log(now)
+if(now>6 && now<19){
+    //check day shift button;
+}
+// else check niteshift button
