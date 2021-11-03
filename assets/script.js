e4b3ce3c0be441270ba08c6ed879e8d803d060f7
Blehjo <89440573+Blehjo@users.noreply.github.com>
2021-11-03 17:54:33 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,17 @@
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-
-// WHEN I refresh the page
-// THEN the saved events persist
-
 //Let's make a timeblock
-var hourColumn = document.querySelectorAll('.hour')
-var hourColumnEl = Array.from(hourColumn);
+var now= new Date().getHours();
+var span = document.querySelectorAll('span')
+var spanEl = Array.from(span);
 var allTimeBlocks = document.querySelectorAll(".col-8");
 var allTimeBlocksEl = Array.from(allTimeBlocks);
 var currentDayEl = document.getElementById('currentDay');
+var saveButton = document.querySelectorAll('i');
+var saveButtonEl = Array.from(saveButton);
+var tasks = document.querySelectorAll("textarea");
+var tasksEl = Array.from(tasks);
 
 function getCurrentDate() {
     var date = moment().format('MMMM Do YYYY');
-    console.log(date);
     currentDayEl.textContent = date;
 } 
 
@@ -22,21 +20,54 @@ $('document').ready(getCurrentDate);
 //We want to call the time column and be able to add a class to it, so that we can apply css to it.
 $('document').ready(changeTense);
 
-var now= new Date().getHours();
-var span = document.querySelectorAll('span')
-var spanEl = Array.from(span);
-
-for (i = 0; i < allTimeBlocksEl.length; i++) {
-    allTimeBlocksEl[i].classList.add('future')
-}
-
-function changeTense () {
-    for (i = 0; i < spanEl.length; i++)
+function changeTense() {
+    var timerInterval = setInterval(function() {
+        for (i = 0; i < spanEl.length - 1; i++)
         if (now >= spanEl[i].id && now < spanEl[(i + 1)].id) {
             allTimeBlocksEl[i].classList.remove('future');
             allTimeBlocksEl[i].classList.add('present');
         } else if (now > spanEl[i].id) {
             allTimeBlocksEl[i].classList.remove('future');
             allTimeBlocksEl[i].classList.add('past');
+        } else allTimeBlocksEl[i].classList.add('future')
+
+        if (now > 18) {
+            clearInterval(timerInterval);
         }
+        
+    }, 1000);
 }
+
+// for (i = 0; i < saveButtonEl.length; i++) 
+// saveButtonEl[i].addEventListener("click", function(event) {
+//     event.preventDefault();
+
+//     localStorage.setItem("tasks", JSON.stringify(tasksEl));
+//     renderLastRegistered();
+// }
+// );
+
+// function renderLastRegistered() {
+//     for (i = 0; i < tasksEl.length; i++)
+//     var area = JSON.parse(localStorage.getItem("tasks"));
+//     console.log(area)
+// }
+var saveBtn = $('.saveBtn');
+
+var calenderInput = localStorage.getItem("dataObject") || "[]";
+var calenderInputArr = JSON.parse(calenderInput);
+
+// loop through each item in the array, grab each item to then display if there is an item
+for (var i = 0; i < calenderInputArr.length; i++) {
+    var timeID = calenderInputArr[i].time
+    var valueVal = calenderInputArr[i].value
+    $('#' + timeID).val(valueVal);
+}
+
+saveBtn.on("click", function () {
+    var time = $(this).siblings(".col-8").attr("id")
+    var text = $(this).siblings(".col-8").val()
+    calenderInputArr.push({time: time, value: text })
+    // console.log(calenderInputArr)
+    localStorage.setItem("dataObject", JSON.stringify(calenderInputArr))
+})
